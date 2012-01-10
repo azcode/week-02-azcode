d5bcb364258d6a755c9bf68806cf2b1be29f8596
Uddin <muddin200@DIVWL-34JHAGUA.(none)>
2012-01-10 12:35:22 -0500

    Added my working test

---
+++ spec/exercise_spec.rb
@@ -0,0 +1,9 @@
+describe "Integer" do
+  context "checks if the integer is odd" do
+    
+    it "validates if the integer is odd or not" do
+      14.odd?.should eq false
+    end
+    
+  end
+end
