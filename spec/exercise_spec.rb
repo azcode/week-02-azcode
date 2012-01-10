fe7aa817c821a46c34f81b901a705b60009cc23f
Uddin <muddin200@DIVWL-34JHAGUA.(none)>
2012-01-10 12:46:47 -0500

    fix this test

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -2,7 +2,7 @@ describe "Integer" do
   context "checks if the integer is odd" do
     
     it "validates if the integer is odd or not" do
-      14.odd?.should eq false
+      14.odd?.should eq true
     end
     
   end
